85ddd2be9eed2888445c3bf68161cc0c92e1c60b
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2021-08-24 10:26:54 +0530

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,95 @@
-use structopt::StructOpt;
-use exitfailure::{ExitFailure};
+use exitfailure::ExitFailure;
+use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
-use reqwest:: Url;
+use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
-	city: String,
-	country_code: String,
+    city: String,
+    country_code: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
- struct Forecast {
-	 coord: Coord,
-	 weather: Weather,
-	 base: String,
-	 main: Temps,
-	 visibility: i32,
-	 wind: Wind,
-	 clouds: Clouds,
-	 dt: i32,
-	 sys: Sys,
-	 timezone: i32,
-	 id: i32,
-	 name: String,
-	 cod: i32,
- }
+struct Forecast {
+    coord: Coord,
+    weather: Weather,
+    base: String,
+    main: Temps,
+    visibility: i32,
+    wind: Wind,
+    clouds: Clouds,
+    dt: i32,
+    sys: Sys,
+    timezone: i32,
+    id: i32,
+    name: String,
+    cod: i32,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Coord {
-	lon: f64,
-	lat: f64,
+    lon: f64,
+    lat: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Weather{
-	details: Details
+struct Weather {
+    details: Details,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Details{
-	id: i32,
-	main: String,
-	description: String,
-	icon: String,
+struct Details {
+    id: i32,
+    main: String,
+    description: String,
+    icon: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Temps{
-	temp: f64,
-	feels_like: f64,
-	temp_min: f64,
-	temp_max: f64,
-	pressure: i32,
-	humidity: i32,
+struct Temps {
+    temp: f64,
+    feels_like: f64,
+    temp_min: f64,
+    temp_max: f64,
+    pressure: i32,
+    humidity: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Wind {
-	speed: f64,
-	deg: i32,
+    speed: f64,
+    deg: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Clouds {
-	all: i32,
+    all: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Sys{
-	r#type: i64,
-	id: i32,
-	country: String,
-	sunrise: i32,
-	sunset: i32,
+struct Sys {
+    r#type: i64,
+    id: i32,
+    country: String,
+    sunrise: i32,
+    sunset: i32,
 }
 
-impl Forecast{
-	async fn get(city: &String, country_code: &String) -> Result<Self, ExitFailure>{
-		let url=format!("http://api.openweathermap.org/data/2.5/weather?q={},{}&appid=d58a6b151ec9cb168b083662ad0ceb53", city, country_code);
-		let url :Url = Url::parse(&*url)?;
+impl Forecast {
+    async fn get(city: &String, country_code: &String) -> Result<Self, ExitFailure> {
+        let url=format!("http://api.openweathermap.org/data/2.5/weather?q={},{}&appid=d58a6b151ec9cb168b083662ad0ceb53", city, country_code);
+        let url: Url = Url::parse(&*url)?;
 
-		let resp=reqwest::get(url)
-		.await?
-		.json::<Forecast>()
-		.await?;
-		Ok(resp)
-	}
-
+        let resp = reqwest::get(url).await?.json::<Forecast>().await?;
+        Ok(resp)
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), ExitFailure>{
+async fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args(); //parse and put it in args
-	let response :Forecast = Forecast::get(&args.city, &args.country_code).await?;
+    let response: Forecast = Forecast::get(&args.city, &args.country_code).await?;
 
-	println!("our city: {} our country code {}, Humidity: {}%, Feels Like: {}, Minimum Temperature: {}, Maximum Temperature: {}", args.city, args.country_code, response.main.humidity, response.main.feels_like, response.main.temp_max, response.main.temp_min);
-	Ok(())
+    println!("our city: {} our country code {}, Humidity: {}%, Feels Like: {}, Minimum Temperature: {}, Maximum Temperature: {}", args.city, args.country_code, response.main.humidity, response.main.feels_like, response.main.temp_max, response.main.temp_min);
+    Ok(())
 }
